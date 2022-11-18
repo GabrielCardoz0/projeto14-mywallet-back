f144b63583f034521f5493531f9b449afbf3486e
Gabriel Cardozo <gabriel.scardozo7@gmail.com>
2022-11-18 15:32:19 -0300

    feat: import uuid, bcrypt. Cadastro de novos usuarios criado

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import cors from "cors"
 import dayjs from "dayjs";
 import Joi from "joi";
 import { MongoClient } from "mongodb";
+import bcrypt from "bcrypt"
+import {v4 as uuid} from "uuid"
 
 
 const app = express();
@@ -10,9 +12,14 @@ app.use(express.json());
 app.use(cors());
 
 
-const userSchema = Joi.object({
+const newUserSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email({tlds:{allow:false}}).required(),
+    password: Joi.required()
+  });
+
+  const userSchema = Joi.object({
+    name:Joi.string().required(),
     password: Joi.required()
   });
 
@@ -32,14 +39,45 @@ mongoClient.connect().then(() => {
   });
 
 //metodos da API:
+//Metodos: post login, post register, put  
+
+app.post("/register", ( async (req, res) => {
+    try{
 
 
+        //verificar se o body está correto:
+        const verify = newUserSchema.validate(req.body);
 
-// app.post("/register", ( (req, res) => {
-//     res.send("ola, você fez um post")
-//     console.log(req.body);
-//     console.log(req.headers);
-// }));
+        if(verify.error){
+            const arr = verify.error.details.map(d => d.message)
+            return res.status(422).send(arr)
+        } 
+
+        //Verificar se o email já está cadastrado no banco de dados:
+        const newUserVerify = await db.collection('users').findOne({email:req.body.email});
+        
+        if(newUserVerify){
+            return res.status(409).send("email já cadastrado");
+        }
+
+        //Inserir novo usuario no DB:
+        const senhaCryptografada = bcrypt.hashSync(req.body.password, 10)
+
+        const newUser = {
+            name:req.body.name,
+            email:req.body.email,
+            password:senhaCryptografada,
+            inputs:[]
+        } 
+
+        await db.collection("users").insertOne(newUser);
+
+    }catch(error){
+        console.log(error)
+    };
+
+    res.send(201)
+}));
 
 
 //post users: O usuario cria um novo user, enviando um objeto com nome, email, senha e senharepetida. Recebe um token e o status 201. A api cria um novo usuario com nome, email, senha (encriptografada), tokenId, e array de inputs 
